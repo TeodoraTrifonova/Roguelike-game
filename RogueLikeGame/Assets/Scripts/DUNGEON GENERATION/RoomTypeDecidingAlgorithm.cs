760c99ff09a88538c2de6fa0cfa2194ec05d91df
Petar Dobrev <peterdobrevxd@gmail.com>
2022-12-06 11:35:02 +0200

    Added the dungeon generation script
    ADDED:
    - dungeon generation scripts:
    * single room generation
    * rooms first generation of a dungeon (will be using this one)
    - couple of sorting layers and normal layers
    
    FIXED:
    - actors' box collisions
    - armed skeleton sprite (had pixels that were not part of the sprite)
    - tweaked some settings on the prefabs

---
+++ RogueLikeGame/Assets/Scripts/DUNGEON GENERATION/RoomTypeDecidingAlgorithm.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class RoomTypeDecidingAlgorithm
+{
+    public static void ChooseStartingRoom(List<Room> rooms)
+    {
+        int minNumberOfRooms = 1;
+        ShuffleRooms(rooms);
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].NeighbourRooms.Count == minNumberOfRooms)
+            {
+                if (rooms[i] is BossRoom) continue;
+                rooms[i] = new BaseRoom(rooms[i]); // starting room
+                Debug.Log($"Created a base room - room {rooms[i].RoomNumber}");
+                break;
+            }
+        }
+    }
+
+    public static void ChooseBossRoom(List<Room> rooms)
+    {
+        int minNumberOfRooms = 1;
+        ShuffleRooms(rooms);
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].NeighbourRooms.Count == minNumberOfRooms)
+            {
+                if (rooms[i] is BaseRoom) continue;
+                rooms[i] = new BossRoom(rooms[i]); // starting room
+                Debug.Log($"Created a base room - room {rooms[i].RoomNumber}");
+                break;
+            }
+        }
+    }
+
+    public static void ShuffleRooms<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
+
